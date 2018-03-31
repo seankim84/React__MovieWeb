f5f8d576ae954eb91313f8990ee33279e5b4988e
Rebekah Park <rebekahpark@Rebekahs-MacBook-Pro.local>
2018-03-31 11:24:28 +0700

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root')); //ReactDom은 App.js를 render한다 (id가 root인 곳에)
-registerServiceWorker();
